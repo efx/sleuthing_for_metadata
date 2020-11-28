fc20187eb940a55dea96614bc1307653809c7db8
Eli Flanagan <efx@users.noreply.github.com>
2020-11-28 10:31:23 -0500

    edit other file

--- snippets/exif_image_author.sh
+++ snippets/exif_image_author.sh
@@ -8,6 +8,3 @@ exiftool -createdate EliFlanagan.jpeg
 
 # what device took this picture?
 exiftool -make -model EliFlanagan.jpeg
-
-# combine these to tell a story
-exiftool -p 'On $createDate, someone took this photograph with a $model.'  EliFlanagan.jpeg
